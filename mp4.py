c97a7e7ce9d3901f3aad1ee3f2a03196c5c4e657
asukawatanabee <54177633+asukawatanabee@users.noreply.github.com>
2019-12-07 16:53:18 +0800

    Add files via upload

---
+++ mp4.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec  1 23:14:24 2019
+
+@author: asukawatanabe
+"""
+import math 
+from matplotlib import pyplot as plt
+def simulate_projectile_motion(s0,v0,theta0,ax,ay):
+    
+    
+    if ay==0:
+        print("Error: vertical acceleration cannot be 0")
+        return  
+    
+    x_vals = [] 
+    y_vals = [] 
+    v0x = v0*math.cos(theta0*(3.141/180)) 
+    v0y = v0*math.sin(theta0*(3.141/180)) 
+    
+    t = 0
+    x = 0
+    y = s0 
+    delta = 0.0001 
+    
+    x_vals.append(x)
+    y_vals.append(y)
+    
+    while(True):
+        t = t+delta
+        x = v0x*t + (0.5)*ax*(t*t)
+        y = v0y*t + (0.5)*ay*(t*t) + s0
+        
+        x_vals.append(x)
+        y_vals.append(y)
+        
+        if y < delta:
+            break
+        
+    plt.plot(x_vals,y_vals)  
+    ax = plt.gca()
+    ax.set_ylim([0,max(y_vals)])
+    plt.xlabel('X axis')
+    plt.ylabel('Y axis')
+    plt.title('Simulation of projectile motion')
+    plt.grid()
+    plt.show()
+        
+if __name__ == '__main__':
+   s0 = 8 # in meters
+   v0 = 14 # in meters per sec
+   theta0 = 65 # in degrees
+   ax = 0 # in meters per second squared
+   ay = -9.81 # in meters per second squared
+   simulate_projectile_motion(s0,v0,theta0,ax,ay)  
